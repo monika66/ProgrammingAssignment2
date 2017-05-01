15085bc8b148e498d24cd1f770bcbed095e97b06
Monika Hunkeler <Monika.Hunkeler@vddatos.com>
2017-05-01 02:34:09 +0200

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+##
+## File       cachematrix.R
+##
+## Coursera   Data Science Specialization
+## course 2   R programming, 
+## week 3     peer-graded Assignment
+## student    Monika Hunkeler
+## date       30.4.2017
+##
+## Programming Assignment 2: Lexical Scoping - Caching the Inverse of a Matrix
+##
+## The function 'makeCacheMatrix' of the file 'cachematrix.R' creates a special 
+## "matrix" object that can cache its inverse. If the inverse has already been 
+## calculated and the matrix has not changed, then the function 'cacheSolve' 
+## should retrieve the inverse from the cache.
 
-## Write a short comment describing this function
+## The function 'makeCacheMatrix' caches a matrix x in the memory and returns
+## a list of methods to get and set data from matrix x and the invers of x 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        minv <- NULL
+        set <- function(y) {
+                # assign data from the parent environment
+                x <<- y
+                minv <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) minv <<- solve
+        getsolve <- function() minv
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## The function 'cacheSolve' returns a matrix that is the inverse of matrix x
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        # if the inverse of the same matrix was calculated before 
+        # return 'minv' with the cached values from the memory 
+        minv <- x$getsolve()
+        if(!is.null(minv)) {
+                message("getting cached data")
+                return(minv)
+        }
+        # else get the matrix x and calculate the invers of x
+        # cache invers x in the memory and assign it to 'minv'
+        data <- x$get()
+        minv <- solve(data, ...)
+        x$setsolve(minv)
+        minv
 }
